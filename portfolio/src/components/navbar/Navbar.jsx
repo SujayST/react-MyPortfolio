ab50ce3ee42c961449cc52f018fc39f7a14996fb
sujaytadahal <47018477+sujaytadahal@users.noreply.github.com>
2021-06-01 00:52:24 +0530

    navbar

--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -1,12 +1,20 @@
 import "./navbar.scss"
+import { Button } from '@material-ui/core';
+
+
 
 export default function Navbar() {
     return (
         <div className="navbar">
             <div className="wrapper">
                 <div className="left">
-                    <h1>NavBar</h1>
-                    <a href="#home">home</a>
+                   <h1> MyPortfolio </h1>
+                </div>
+                <div className="right">
+                    <Button className="button" href="#home" >Home</Button>
+                    <Button className="button" href="#aboutme" >Aboutme</Button>
+                    <Button className="button" href="#experience" color="secondary">Experience</Button>
+                    <Button className="button" href="#projects" color="white">Projects</Button>
                 </div>
 
             </div>
