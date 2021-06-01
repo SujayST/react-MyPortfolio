521f8b036d2c07debdd10a9df72d10ae8fa77cce
SonaliKabadi11 <sonaliak1999@gmail.com>
2021-06-01 10:54:44 +0530

    home and aboutme separated

--- portfolio/src/components/aboutme/Aboutme.jsx
+++ portfolio/src/components/aboutme/Aboutme.jsx
@@ -1,9 +1,23 @@
-import "./aboutme.scss"
+import "./aboutme.scss";
+
+import video from "../../assets/Videos/butter.mp4";
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Aboutme() {
     return (
-        <div className="aboutme" id='aboutme'>
-            <h1>AboutME</h1>
+        <div className="con">
+            <div className="about">
+                <h1 className="text-left font-weight-bold " style={{color: "#CF6679"}}>About Me</h1> <br/>
+                <h3 className="text-left font-weight-bold text-white">I'm Sonali Kabadi</h3>
+                <h6 className="text-left  text-white">   I'm currently a third year Engineering student majoring in
+                Computer Science in Kle Technological University, Hubli, Karnataka, Inida.
+                    I'm passionate Frontend developer who focuses on writing clean, elegant and efficient code.User Interface Desinger designing beautifull and functional user interfaces.
+                </h6>
+            </div>
+            <div className="video">
+                <video src={video} width="500" height="200" controls style={{marginTop:80}}>
+                </video>
+            </div>
         </div>
     )
 }
--- portfolio/src/components/home/Home.jsx
+++ portfolio/src/components/home/Home.jsx
@@ -1,7 +1,6 @@
 import "./home.scss";
 import image from "../../assets/images/pb.png";
 import me from "../../assets/images/me.png";
-import video from "../../assets/Videos/butter.mp4";
 import 'bootstrap/dist/css/bootstrap.css';
 
 
@@ -21,20 +20,7 @@ export default function Home() {
 
         </div>
         <br/><br/>
-        <div className="con">
-            <div className="about">
-                <h1 className="text-left font-weight-bold " style={{color: "#CF6679"}}>About Me</h1> <br/>
-                <h3 className="text-left font-weight-bold text-white">I'm Sonali Kabadi</h3>
-                <h6 className="text-left  text-white">   I'm currently a third year Engineering student majoring in
-                Computer Science in Kle Technological University, Hubli, Karnataka, Inida.
-                    I'm passionate Frontend developer who focuses on writing clean, elegant and efficient code.User Interface Desinger designing beautifull and functional user interfaces.
-                </h6>
-            </div>
-            <div className="video">
-                <video src={video} width="500" height="200" controls style={{marginTop:80}}>
-                </video>
-            </div>
-        </div>
+        
         </section>
     )
 }
