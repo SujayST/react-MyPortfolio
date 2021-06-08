90ede307506602f4b58c90b0c9625019eb8c08e4
shivaninazare <shivaninazare1234@gmail.com>
2021-06-08 10:44:26 +0530

    contact links

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
 import "./contact.scss";
 import shake from "../../assets/images/shake.png";
-import globe  from "../../assets/images/globe.png"; 
 import linkedin from "../../assets/images/linkedin.png";
-import youtube from "../../assets/images/youtube.png";
 import mobile from "../../assets/images/mobile.png";
 import twitter  from "../../assets/images/twitter.png";
-
 import github from "../../assets/logos/github-logo.svg";
-
 import insta from "../../assets/logos/insta.png";
+
 export default function Contact() {
   const [message, setMessage] = useState(false);
 
@@ -29,27 +26,27 @@ export default function Contact() {
       <div className="row">
             <div className="column">
             <a href="https://www.linkedin.com/in/sujay-tadahal/" >
-                    <img className="logo" src={twitter} width="30" height="30"  alt="Linkedin" />
+                    <img className="logo" src={twitter} width="25" height="30"  alt="Twitter" />
                 </a>
             </div>
-            <div className="column">
-            <a href="https://www.linkedin.com/in/sujay-tadahal/" >
-                    <img className="logo" src={globe} width="30" height="30"  alt="Linkedin" />
-                </a>
-             </div>
              <div className="column">
              <a href="https://www.linkedin.com/in/sujay-tadahal/" >
-                    <img className="logo" src={linkedin} width="30" height="30"  alt="Linkedin" />
+                    <img className="logo" src={linkedin} width="25" height="30"  alt="Linkedin" />
                 </a>
             </div>
             <div className="column" id="s1" >
                 <a href="https://www.linkedin.com/in/sujay-tadahal/" >
-                    <img className="logo" src={mobile} width="30" height="30" alt="Linkedin" />
+                    <img className="logo" src={mobile} width="25" height="30" alt="Mobile" />
                 </a>
             </div>
              <div className="column">
              <a href="https://www.linkedin.com/in/sujay-tadahal/" >
-                    <img className="logo" src={youtube} width="30" height="30"  alt="Linkedin" />
+                    <img className="logo" src={github} width="25" height="30"  alt="Github" />
+                </a>
+             </div>
+             <div className="column">
+             <a href="https://www.linkedin.com/in/sujay-tadahal/" >
+                    <img className="logo" src={insta} width="25" height="30"  alt="Instagram" />
                 </a>
              </div>
          </div>
