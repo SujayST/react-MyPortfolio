f93e8527d2df65c413b533a2386cb93d4860654b
shivaninazare <shivaninazare1234@gmail.com>
2021-06-08 06:24:44 +0530

    kk

--- portfolio/src/components/home/Home.jsx
+++ portfolio/src/components/home/Home.jsx
@@ -10,8 +10,8 @@ export default function Home() {
       <div className="home" id="home" >
         <section className="mt-0">
           <div className="align-self-end ">
-            <h1 className="name1" >Aloha ! </h1> 
-            <h1 className="name">I'm Sonali Kabadi. </h1> <br/>
+            <h1 className="name1" >Aloha! </h1> 
+            <h1 className="name">I'm Sonali Kabadi ..... </h1> <br/>
             <img className="rounded mx-auto d-block" src={me} alt="Oopss!!" /> 
             <h5 className="text-center font-weight-bold " style={{color: "#CF6679"}}> Web Developer | Data Analyst  </h5><br/>
           </div>
